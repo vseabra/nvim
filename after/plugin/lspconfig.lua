a980ba7e3b9f59ed4023a65e8f474e51ffc63030
carvhal <vseabracarvalho@gmail.com>
2023-12-22 22:38:49 -0300

    fix: fix selene undefined global 'vim' warning

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,32 +1,37 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require("lspconfig").lua_ls.setup({
+local lspconfig = require('lspconfig')
+
+lspconfig.lua_ls.setup {
   settings = {
     Lua = {
       runtime = {
-        version = "LuaJIT",
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
       },
       workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-        },
-        diagnostics = {
-          globals = {
-            "vim",
-            "require",
-          },
-        },
-        telemetry = {
-          enable = false,
-        },
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
       },
     },
   },
-})
-require("lspconfig").gopls.setup({})
-require("lspconfig").jsonls.setup({
+}
+
+lspconfig.gopls.setup({})
+lspconfig.jsonls.setup({
   capabilities = capabilities,
 })
 
