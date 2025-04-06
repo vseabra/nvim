fcc6195d6b288da21ad87917623cbff54c129152
vseabra <vseabracarvalho@gmail.com>
2025-04-06 16:49:28 -0300

    feat: rust analyzer

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -3,33 +3,33 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspconfig = require("lspconfig")
 
-lspconfig.gdscript.setup{}
+lspconfig.gdscript.setup({})
 
 lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          "vim",
-          "require",
-        },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
 lspconfig.gopls.setup(capabilities)
@@ -38,13 +38,27 @@ lspconfig.svelte.setup({})
 -- lspconfig.tailwindcss.setup({})
 
 lspconfig.jsonls.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 
 -- lspconfig.tsserver.setup(capabilities)
 lspconfig.ts_ls.setup(capabilities)
 
 lspconfig.gdscript.setup(capabilities)
+
+lspconfig.rust_analyzer.setup({
+	completion = {
+		callable = { snippets = "fill_arguments" },
+		fullFunctionSignatures = { enable = true },
+	},
+	procMacro = { enable = true },
+	check = {
+		command = "clippy",
+	},
+	cargo = {
+		features = "all",
+	},
+})
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
