2fae98739bb30610e24e32dcd7ace3429e3e290d
Vinícius Seabra de Carvalho <vseabra@protonmail.com>
2026-02-08 13:55:40 -0300

    add copilot back

---
+++ lua/seabra/plugins/misc/copilotChat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+    },
+  },
+}
